faaf07dfd1aa195ab73ad0e9218322356dbacd87
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-10 21:29:50 -0400

    add onChange/onSubmit event handler functions to RegisterForm component

--- client/src/components/register/RegisterForm.js
+++ client/src/components/register/RegisterForm.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function RegisterForm() {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  function onChange(event) {
+    setCredentials({
+      ...credentials,
+      [event.target.name] : event.target.value
+    })
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setCredentials({
+      username: '',
+      password: ''
+    })
+    console.log('credentials: ', credentials)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input 
           type='text'
+          name='username'
+          onChange={onChange}
+          value={credentials.username}
+          placeholder='username'
         />
         <input 
-          type='text'
+          type='password'
+          name='password'
+          onChange={onChange}
+          value={credentials.password}
+          placeholder='password'
         />
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
