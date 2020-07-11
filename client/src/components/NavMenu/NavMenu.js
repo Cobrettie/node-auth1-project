81447873bb8d884d966bb4fc6b1bae16aa75622b
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-10 20:56:25 -0400

    configure navmenu component + delete logo.svg + configure boilerplate registerform component + edit readme

---
+++ client/src/components/NavMenu/NavMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function NavMenu() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <a href="../register/RegisterForm.js">register</a>
+          <a href="#">log in</a>
+          <a href="#">users</a>
+        </ul>
+      </nav>
+    </div>
+  )
+}
---
+++ client/src/components/register/RegisterForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function RegisterForm() {
+  return (
+    <div>
+      <form>
+        <input 
+          type='text'
+        />
+        <input 
+          type='text'
+        />
+      </form>
+    </div>
+  )
+}
